9d6ae2c6d7b2ec9688fbef15a917fdf73be0a671
Locky Keaney <lachlan.keaney@kolmeo.com>
2022-10-24 14:27:20 +1100

    updated react and installed RTL

--- src/ScoreBoard.test.js
+++ src/ScoreBoard.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ScoreBoard from './ScoreBoard';
-import Frame from './Frame';
-import {shallow} from 'enzyme';
+import React from "react";
+import ScoreBoard from "./ScoreBoard";
+import Frame from "./Frame";
 
-it('should render ten Frame components', () => {
-  var wrapper = shallow(<ScoreBoard/>)
-  expect(wrapper.find(Frame).length).toEqual(10);
+import { render, screen } from "@testing-library/react";
+
+it("should render ten Frame components", () => {
+  render(<ScoreBoard />);
+  expect(screen.getAllByRole("Frame").length).toEqual(10);
 });
